31bdd0d5437b4b9c2823e9fe318e5ae4352bdc49
JLenz98 <j.70lenz@gmail.com>
2019-02-23 18:19:46 -0600

    Variables and User IO

---
+++ fullname.rb
@@ -0,0 +1,7 @@
+puts "First Name:"
+nameFirst = gets.chomp
+
+puts "Last Name:"
+nameLast = gets.chomp
+
+puts nameFirst + " " + nameLast
---
+++ number_multiplier.rb
@@ -0,0 +1,5 @@
+puts "Enter two numbers to be multiplied:"
+number1 = gets.chomp.to_i
+number2 = gets.chomp.to_i
+
+puts number1 * number2
